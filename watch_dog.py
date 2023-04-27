88741e8c1f8978eddcdb9c146fc083517682e776
mha4065 <71762165+mha4065@users.noreply.github.com>
2023-04-27 12:34:55 +0330

    new features

--- watch_dog.py
+++ watch_dog.py
@@ -1,16 +1,19 @@
 import requests
 import pymongo
+import re
 from time import sleep
 from discord_webhook import DiscordWebhook, DiscordEmbed
 
 
 platforms = ['hackerone', 'bugcrowd', 'intigriti', 'yeswehack']
 
-HACKERONE = "https://raw.githubusercontent.com/Osb0rn3/bugbounty-targets/4256468efce8d8aa9b3a0f00d3653fe50bae95ab/programs/hackerone.json"
+PATTERN = r"(?:(?:https?|ftp):\/\/)?(?:[\w-]+\.)+[a-z]{2,}(?::\d+)?(?:\/\S*)?"
+
+HACKERONE = "https://raw.githubusercontent.com/Osb0rn3/bugbounty-targets/main/programs/hackerone.json"
 BUGCROWD = "https://raw.githubusercontent.com/Osb0rn3/bugbounty-targets/main/programs/bugcrowd.json"
 INTIGRITI = "https://raw.githubusercontent.com/Osb0rn3/bugbounty-targets/main/programs/intigriti.json"
 YESWEHACK = "https://raw.githubusercontent.com/Osb0rn3/bugbounty-targets/main/programs/yeswehack.json"
-
+PATH = "https://raw.githubusercontent.com/Osb0rn3/bugbounty-targets/main/programs/"
 
 def get_data(url:str):
     return requests.get(url).json()
@@ -49,18 +52,25 @@ def check_changes(platform:str):
         new_results = cleaner(get_data(HACKERONE), platform)
         for program in new_results:
             old_data = collection.find_one({"handle": program["handle"]})
+            
             if old_data == None:
+                in_scopes = "\n".join(program["in_scope"])
+                push(program["logo"], program["name"], program["program_url"], platform, in_scopes, program["offers_bounties"], "New Program")
                 collection.insert_one(program)
                 continue
+            
             for in_scope in program["in_scope"]:
                 if in_scope not in old_data["in_scope"]:
                     collection.update_one({"handle": program["handle"]}, {"$push": {"in_scope": in_scope}})
-                    push(program["logo"], program["name"], program["program_url"], platform, in_scope)
+                    push(program["logo"], program["name"], program["program_url"], platform, in_scope, program["offers_bounties"], "New Scope")
                     print(f"new scope found! {in_scope}")
+                    
             for out_of_scope in program["out_of_scope"]:
                 if out_of_scope not in old_data["out_of_scope"]:
-                    print(f"new out of scope found! {out_of_scope}")
-                    collection.update_one({"handle": program["handle"]}, {"$push": {"out_of_scope": {out_of_scope}}})
+                    print(f"new out of scope found! out_of_scope")
+                    push(program["logo"], program["name"], program["program_url"], platform, out_of_scope, program["offers_bounties"], "Out of Scope")
+                    collection.update_one({"handle": program["handle"]}, {"$push": {"out_of_scope": out_of_scope}})
+                    
     elif platform == 'bugcrowd':
         collection = db["bugcrowd"]
         # get new data
@@ -68,18 +78,23 @@ def check_changes(platform:str):
         for program in new_results:
             old_data = collection.find_one({"code": program["code"]})
             if old_data == None:
+                in_scopes = "\n".join(program["in_scope"])
+                push(program["logo"], program["name"], program["program_url"], platform, in_scopes, program["offers_bounties"], "New Program")
+                # New program
                 collection.insert_one(program)
                 continue
             for in_scope in program["in_scope"]:
                 #print(in_scope)
                 if in_scope not in old_data["in_scope"]:
                     collection.update_one({"code": program["code"]}, {"$push": {"in_scope": in_scope}})
-                    push(program["logo"], program["name"], program["program_url"], platform, in_scope)
+                    push(program["logo"], program["name"], program["program_url"], platform, in_scope, program["offers_bounties"], "New Scope")
                     print(f"new scope found! {in_scope}")
             for out_of_scope in program["out_of_scope"]:
                 if out_of_scope not in old_data["out_of_scope"]:
-                    print(f"new out of scope found! {out_of_scope}")
-                    collection.update_one({"code": program["code"]}, {"$push": {"out_of_scope": {out_of_scope}}})
+                    print(f"new out of scope found! out_of_scope")
+                    push(program["logo"], program["name"], program["program_url"], platform, out_of_scope, program["offers_bounties"], "Out of Scope")
+                    collection.update_one({"code": program["code"]}, {"$push": {"out_of_scope": out_of_scope}})
+                    
     elif platform == 'intigriti':
         collection = db["intigriti"]
         # get new data
@@ -87,18 +102,22 @@ def check_changes(platform:str):
         for program in new_results:
             old_data = collection.find_one({"handle": program["handle"]})
             if old_data == None:
+                in_scopes = "\n".join(program["in_scope"])
+                push(program["logo"], program["name"], program["program_url"], platform, in_scopes, program["offers_bounties"], "New Program")
                 collection.insert_one(program)
                 continue
             for in_scope in program["in_scope"]:
                 #print(in_scope)
                 if in_scope not in old_data["in_scope"]:
                     collection.update_one({"handle": program["handle"]}, {"$push": {"in_scope": in_scope}})
-                    push(program["logo"], program["name"], program["program_url"], platform, in_scope)
+                    push(program["logo"], program["name"], program["program_url"], platform, in_scope, program["offers_bounties"], "New Scope")
                     print(f"new scope found! {in_scope}")
             for out_of_scope in program["out_of_scope"]:
                 if out_of_scope not in old_data["out_of_scope"]:
-                    print(f"new out of scope found! {out_of_scope}")
-                    collection.update_one({"handle": program["handle"]}, {"$push": {"out_of_scope": {out_of_scope}}})
+                    print(f"new out of scope found! out_of_scope")
+                    push(program["logo"], program["name"], program["program_url"], platform, out_of_scope, program["offers_bounties"], "Out of Scope")
+                    collection.update_one({"handle": program["handle"]}, {"$push": {"out_of_scope": out_of_scope}})
+    
     elif platform == 'yeswehack':
         collection = db["yeswehack"]
         # get new data
@@ -106,17 +125,19 @@ def check_changes(platform:str):
         for program in new_results:
             old_data = collection.find_one({"slug": program["slug"]})
             if old_data == None:
+                in_scopes = "\n".join(program["in_scope"])
+                push(program["logo"], program["title"], program["program_url"], platform, in_scopes, program["offers_bounties"], "New Program")
                 collection.insert_one(program)
                 continue
             for in_scope in program["in_scope"]:
                 if in_scope not in old_data["in_scope"]:
                     collection.update_one({"slug": program["slug"]}, {"$push": {"in_scope": in_scope}})
-                    push(program["logo"], program["title"], program["program_url"], platform, in_scope)
+                    push(program["logo"], program["title"], program["program_url"], platform, in_scope, program["offers_bounties"], "New Scope")
                     print(f"new scope found! {in_scope}")
             # for out_of_scope in program["out_of_scope"]:
             #     if out_of_scope not in old_data["out_of_scope"]:
-            #         print(f"new out of scope found! {out_of_scope}")
-            #         collection.update_one({"slug": program["slug"]}, {"$push": {"out_of_scope": {out_of_scope}}})
+            #         print(f"new out of scope found! out_of_scope")
+            #         collection.update_one({"slug": program["slug"]}, {"$push": {"out_of_scope": out_of_scope}})
     else:
         pass
                 
@@ -127,16 +148,18 @@ def cleaner(data:dict, platform:str):
         for program in data:
             program_name = program["attributes"]["name"]
             program_handle = program["attributes"]["handle"]
-            programScope = program["relationships"]["structured_scopes"]["data"]
-            if len(programScope) == 0:
+            program_scopes = program["relationships"]["structured_scopes"]["data"]
+            offers_bounties = program["attributes"]["offers_bounties"]
+            if len(program_scopes) == 0:
                 continue
             programs_dict = {"name":program_name,
                              "handle": program_handle,
+                             "offers_bounties": offers_bounties,
                              "program_url": "https://hackerone.com/" + program_handle,
                              "logo": program["attributes"]["profile_picture"],
                              "in_scope": [],
                              "out_of_scope": []}
-            for x in programScope:
+            for x in program_scopes:
                 if x["attributes"]["eligible_for_submission"]:
                     programs_dict["in_scope"].append(x["attributes"]["asset_identifier"])
                 else:
@@ -148,16 +171,19 @@ def cleaner(data:dict, platform:str):
         for program in data:
             program_name = program["name"]
             program_code = program["code"]
-            programScope = program["target_groups"]
-            if len(programScope) == 0:
+            program_scopes = program["target_groups"]
+            offers_bounties = True if program["license_key"] == "bug_bounty" else False
+            program["license_key"]
+            if len(program_scopes) == 0:
                 continue
             programs_dict = {"name":program_name,
+                             "offers_bounties": offers_bounties,
                              "code": program_code,
                              "program_url": "https://bugcrowd.com/" + program_code,
                              "logo": program["logo"],
                              "in_scope": [],
                              "out_of_scope": []}
-            for x in programScope:
+            for x in program_scopes:
                 if x["in_scope"] == True:
                     for y in x["targets"]:
                         programs_dict["in_scope"].append(y["uri"])
@@ -172,21 +198,28 @@ def cleaner(data:dict, platform:str):
             program_name = program["name"]
             program_company_handle = program["companyHandle"]
             program_handle = program["handle"]
-            programScope = program["domains"]
-            if len(programScope) == 0:
+            program_scopes = program["domains"]
+            if len(program_scopes) == 0:
                 continue
+            offers_bounties = True if program["maxBounty"]["value"] > 0 else False
             programs_dict = {"name":program_name,
+                             "offers_bounties": offers_bounties,
                              "companyHandle": program_company_handle,
                              "handle": program_handle,
                              "program_url": "https://app.intigriti.com/programs/" + program_company_handle + "/" + program_handle,
                              "logo": "https://bff-public.intigriti.com/file/" + program["logoId"],
                              "in_scope": [],
                              "out_of_scope": []}
-            for x in programScope:
+            for x in program_scopes:
                 if x["description"] and 'Out of scope' in x["description"]:
-                    programs_dict["out_of_scope"].append(x["endpoint"])
+                    # out of scope!
+                    domains = re.findall(PATTERN, x["description"])
+                    for domain in domains:
+                        programs_dict["out_of_scope"].append(domain)
                 else:
+                    # in_scope!
                     programs_dict["in_scope"].append(x["endpoint"])
+                    
             programs.append(programs_dict)
         return programs
     elif platform == 'yeswehack':
@@ -194,16 +227,18 @@ def cleaner(data:dict, platform:str):
         for program in data:
             program_title = program["title"]
             program_slug = program["slug"]
-            programScope = program["scopes"]
-            if len(programScope) == 0:
+            program_scopes = program["scopes"]
+            if len(program_scopes) == 0:
                 continue
+            offers_bounties = True if program["bounty"] else False
             programs_dict = {"title":program_title,
+                             "offers_bounties": offers_bounties,
                              "slug": program_slug,
                              "program_url": "https://yeswehack.com/programs/" + program_slug,
                              "logo": program["thumbnail"]["url"],
                              "in_scope": [],
                              "out_of_scope": []}
-            for x in programScope:
+            for x in program_scopes:
                 if x["scope_type"] == "web-application":
                     programs_dict["in_scope"].append(x["scope"])
                 else:
@@ -215,37 +250,38 @@ def cleaner(data:dict, platform:str):
         return
     
 
-def push(logo_url, program_name, program_url, platform, message:str):
+def push(logo_url, program_name, program_url, platform, message:str, bounty:bool, type:str):
+    webhook = DiscordWebhook(url="<YOUR WEBHOOK>")
     if platform == 'hackerone':
-        webhook = DiscordWebhook(url="https://ptb.discord.com/api/webhooks/1100875059360436364/jFO75ZMDIaQXtERWLjSOoY2xLmxGoFXpbySYJ6aZDad_x282Wqn5R_ynDb2rai3yYgbu")
-        embed = DiscordEmbed(title="HackerOne", description=message, color="ffffff")
+        embed = DiscordEmbed(title=type, description=message, color="ffffff")
         embed.set_thumbnail(url=logo_url)
-        embed.add_embed_field(name="Program:", value=program_name)
-        embed.add_embed_field(name="URL:", value=program_url)             
+        embed.add_embed_field(name="Program:", value=program_name, inline=False)
+        embed.add_embed_field(name="Bounty:", value=bounty, inline=False)
+        embed.add_embed_field(name="URL:", value=program_url, inline=False)             
         webhook.add_embed(embed)
         response = webhook.execute()
     elif platform == 'bugcrowd':
-        webhook = DiscordWebhook(url="https://ptb.discord.com/api/webhooks/1100875059360436364/jFO75ZMDIaQXtERWLjSOoY2xLmxGoFXpbySYJ6aZDad_x282Wqn5R_ynDb2rai3yYgbu")
-        embed = DiscordEmbed(title="Bugcrowd", description=message, color="ff6900")
+        embed = DiscordEmbed(title=type, description=message, color="ff6900")
         embed.set_thumbnail(url=logo_url)
-        embed.add_embed_field(name="Program:", value=program_name)
-        embed.add_embed_field(name="URL:", value=program_url)             
+        embed.add_embed_field(name="Program:", value=program_name, inline=False)
+        embed.add_embed_field(name="Bounty:", value=bounty, inline=False)
+        embed.add_embed_field(name="URL:", value=program_url, inline=False)             
         webhook.add_embed(embed)
         response = webhook.execute()
     elif platform == 'intigriti':
-        webhook = DiscordWebhook(url="https://ptb.discord.com/api/webhooks/1100875059360436364/jFO75ZMDIaQXtERWLjSOoY2xLmxGoFXpbySYJ6aZDad_x282Wqn5R_ynDb2rai3yYgbu")
-        embed = DiscordEmbed(title="Intigriti", description=message, color="4c59a8")
+        embed = DiscordEmbed(title=type, description=message, color="4c59a8")
         embed.set_thumbnail(url=logo_url)
-        embed.add_embed_field(name="Program:", value=program_name)
-        embed.add_embed_field(name="URL:", value=program_url)             
+        embed.add_embed_field(name="Program:", value=program_name, inline=False)
+        embed.add_embed_field(name="Bounty:", value=bounty, inline=False)
+        embed.add_embed_field(name="URL:", value=program_url, inline=False)             
         webhook.add_embed(embed)
         response = webhook.execute()
     elif platform == 'yeswehack':
-        webhook = DiscordWebhook(url="https://ptb.discord.com/api/webhooks/1100875059360436364/jFO75ZMDIaQXtERWLjSOoY2xLmxGoFXpbySYJ6aZDad_x282Wqn5R_ynDb2rai3yYgbu")
-        embed = DiscordEmbed(title="YesWeHack", description=message, color="8f0e0e")
+        embed = DiscordEmbed(title=type, description=message, color="8f0e0e")
         embed.set_thumbnail(url=logo_url)
-        embed.add_embed_field(name="Program:", value=program_name)
-        embed.add_embed_field(name="URL:", value=program_url)             
+        embed.add_embed_field(name="Program:", value=program_name, inline=False)
+        embed.add_embed_field(name="Bounty:", value=bounty, inline=False)
+        embed.add_embed_field(name="URL:", value=program_url, inline=False)             
         webhook.add_embed(embed)
         response = webhook.execute()
     else:
@@ -254,17 +290,8 @@ def push(logo_url, program_name, program_url, platform, message:str):
 
 def main():
     for platform in platforms:
-        if platform == 'hackerone':
-            result = get_data(HACKERONE)
-        elif platform == 'bugcrowd':
-            result = get_data(BUGCROWD)
-        elif platform == 'intigriti':
-            result = get_data(INTIGRITI)
-        elif platform == 'yeswehack':
-            result = get_data(YESWEHACK)
-        else:
-            return
-        
+        URL = PATH + platform + ".json"
+        result = get_data(URL)
         clean_result = cleaner(result, platform)
         insert_db(clean_result, platform)
 
